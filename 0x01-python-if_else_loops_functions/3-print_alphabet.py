fa53a32f7c92646593c51091ddcb108bcc4bebf8
Lynnagneta <lynnetteagneta@gmail.com>
2024-01-02 18:23:36 -0800

    loop for python

--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alphabet in range(97, 123):
+for alphabet in range(97, 122):
     if chr(alphabet) != 'q' and chr(alphabet) != 'e':
         print("{}".format(chr(alphabet)), end="")
