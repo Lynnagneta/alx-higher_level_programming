443d045eb6cd5de1dd7ea5520bdac27c29a6ed85
Lynnagneta <lynnetteagneta@live.com>
2025-01-01 13:45:37 -0800

    commit message

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""
-Lists all cities of the state, using the database
-"""
-import sys
-import MySQLdb
+"""takes in the name of a state as an argument and
+lists all cities of that state,
+using the database hbtn_0e_4_usa"""
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],                     passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+
     cur = db.cursor()
-    cur.execute("""SELECT name FROM cities WHERE (SELECT id FROM states WHERE
-                BINARY name = %s) = state_id""", (sys.argv[4],))
-    states = cur.fetchall()
-    print(", ".join([state[0] for state in states]))
+    cur.execute("SELECT cities.name\
+                FROM cities LEFT JOIN states\
+                ON states.id = cities.state_id\
+                WHERE states.name = %s\
+                ORDER BY cities.id ASC", (sys.argv[4],))
+    rows = cur.fetchall()
+    print(", ".join([row[0] for row in rows]))
     cur.close()
     db.close()
