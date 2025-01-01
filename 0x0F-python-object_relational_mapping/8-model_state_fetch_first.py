0414560c355d8951ae0a8b2fa3f52384faabce1a
Lynnagneta <lynnetteagneta@live.com>
2025-01-01 13:51:36 -0800

    commit message

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-""" print state object """
+"""prints the first State object from the database hbtn_0e_6_usa"""
 
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import sys
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Sessionmaker = sessionmaker(bind=engine)
-    session = Sessionmaker()
-    response = session.query(State).first()
-    if response:
-        print("1: " + response.name)
+
+    session = Session(engine)
+    first = session.query(State).order_by(State.id).first()
+    if first:
+        print("{}: {}".format(first.id, first.name))
     else:
         print("Nothing")
     session.close()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""
-city class
-"""
+"""Definition of the State class"""
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
 
-class City(Base):
-    """ City class """
-    __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement="auto", unique=True)
+class State(Base):
+    """Class State"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True,
+                primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
